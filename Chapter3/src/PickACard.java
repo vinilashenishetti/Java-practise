c430f732f0d108b95566da5dea35038cda19b8a2
Vinila Shenishetti <vinila4us@gmail.com>
2021-05-23 17:38:41 -0700

    test commit

--- Chapter3/src/PickACard.java
+++ Chapter3/src/PickACard.java
@@ -10,6 +10,6 @@ public class PickACard {
 	 String rank[] = {"Ace" , "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "king"};
 	 String suit[] = {"Clubs", "Diamonds", "Hearts", "Spades"};
 	 
-	 System.out.print("The card you picked is " +rank[cardRank] + " of " + suit[cardSuit]);
+	 System.out.print("The card you picked is: " +rank[cardRank] + " of: " + suit[cardSuit]);
  }
 }
